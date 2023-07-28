8fa6180847111adfa3c18c9d764bdbe984e887f4
“sherwin <sherwinatendido17@yahoo.com>
2023-07-28 15:44:38 +0800

    update with scoreboard data file

--- scoreboard.py
+++ scoreboard.py
@@ -8,6 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -16,12 +18,16 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Scoreboard: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Scoreboard: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"Game Over", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as new_data:
+                new_data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+
     def add_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
